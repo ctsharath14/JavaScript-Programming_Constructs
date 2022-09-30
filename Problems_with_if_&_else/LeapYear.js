8a72388f0cfba8d7faac431b229add7e7650e0c4
C T SOMAIAH <102033298+ctsharath14@users.noreply.github.com>
2022-09-30 12:00:23 +0530

    Update LeapYear.js

--- Problems_with_if_&_else/LeapYear.js
+++ Problems_with_if_&_else/LeapYear.js
@@ -1,5 +1,4 @@
-const prompt = require("prompt-sync")();
-let year = prompt("Enter the year : ")
+let year = 2014
 if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 console.log(year + " is a Leap year.");
 else console.log(year + " is not a Leap year.");
